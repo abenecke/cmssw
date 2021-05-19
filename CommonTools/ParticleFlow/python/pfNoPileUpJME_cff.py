db3c1697290ddd3febeae7b952cd8c3fbaaeb9d0
Anna Benecke <anna.benecke@cern.ch>
2021-05-19 10:05:22 +0200

    small fixes

--- CommonTools/ParticleFlow/python/pfNoPileUpJME_cff.py
+++ CommonTools/ParticleFlow/python/pfNoPileUpJME_cff.py
@@ -16,7 +16,7 @@ def adapt(primaryVertexAssociationJME):
   primaryVertexAssociationJME.assignment.OnlyUseFirstDz = True
   from Configuration.Eras.Modifier_phase2_common_cff import phase2_common
   phase2_common.toModify(
-    primaryVertexAssociationJME,
+    primaryVertexAssociationJME.assignment,
     EtaMinUseDz = 4.0
     )
   phase2_common.toModify(
