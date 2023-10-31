2b6a676ca3e7af220e5ba172e0c257c189a762fa
Anna Benecke <anna.benecke@cern.ch>
2023-10-31 08:32:01 +0100

    keeping fromPV == 2 PF with pT > 4 GeV

--- CommonTools/PileupAlgos/plugins/PuppiProducer.cc
+++ CommonTools/PileupAlgos/plugins/PuppiProducer.cc
@@ -285,6 +285,8 @@ void PuppiProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
               pReco.id = 1;
             else if ((fUseDZ) && (std::abs(pReco.eta) >= fEtaMinUseDZ) && (std::abs(pDZ) < fDZCut))
               pReco.id = 1;
+	    else if (tmpFromPV == 2 && (pReco.pt > 4))
+	      pReco.id = 1;
             else if ((fUseDZforPileup) && (std::abs(pReco.eta) >= fEtaMinUseDZ) && (std::abs(pDZ) >= fDZCut))
               pReco.id = 2;
             else if (fUseFromPVLooseTight && tmpFromPV == 1)
@@ -328,6 +330,8 @@ void PuppiProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
               pReco.id = 1;
             else if ((fUseDZ) && (std::abs(pReco.eta) >= fEtaMinUseDZ) && (std::abs(pDZ) < fDZCut))
               pReco.id = 1;
+	    else if (lPack->fromPV() == (pat::PackedCandidate::PVTight) && (pReco.pt > 4))
+	      pReco.id = 1;
             else if ((fUseDZforPileup) && (std::abs(pReco.eta) >= fEtaMinUseDZ) && (std::abs(pDZ) >= fDZCut))
               pReco.id = 2;
             else if (fUseFromPVLooseTight && lPack->fromPV() == (pat::PackedCandidate::PVLoose))
